052a4022da4a9dcd75943148397be98202698d8a
Stephen Bussard <sbussard@users.noreply.github.com>
2018-07-31 11:14:55 -0500

    enable use of environment variables (#10)

---
+++ src/configuration/environment.js
@@ -0,0 +1,24 @@
+export const PROCESS_ENV = process.env; // eslint-disable-line
+export const REQUIRED_VARIABLES = ['GOOGLE_ANALYTICS_ID'];
+
+let findMissingVariable = variable =>
+  Object.keys(PROCESS_ENV).indexOf(variable) === -1;
+let missingVariables = REQUIRED_VARIABLES.filter(findMissingVariable);
+
+if (missingVariables.length > 0) {
+  let sep = '\n\t> ';
+  throw new Error(
+    `Missing environment variables: \n${sep}${missingVariables.join(sep)}\n`
+  );
+}
+
+let toEnvironmentVariableJsonMap = variableName => ({
+  [variableName]: JSON.stringify(PROCESS_ENV[variableName])
+});
+
+let shallowMerge = (previous, current) => ({ ...previous, ...current });
+
+export const ENV = REQUIRED_VARIABLES.map(toEnvironmentVariableJsonMap).reduce(
+  shallowMerge,
+  {}
+);

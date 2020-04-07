1719b92971f37c0da46abf9ba5aad9640ae15783
Stephen Bussard <sbussard@users.noreply.github.com>
2020-04-07 00:51:37 -0700

    Upgrade patterns, migrate to create-react-app (#25)

--- src/index.jsx
+++
@@ -1,8 +0,0 @@
-import React, { Component, PropTypes } from 'react';
-import { render } from 'react-dom';
-
-import '~/theme/base.scss';
-
-import Canvas from '~/component/Canvas';
-
-render(<Canvas />, document.querySelector('.app'));
